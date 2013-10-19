192d42df2f5fd73401dfeaeca292ccb0f9651bc8
drenfr01 <duncanrenfrow@gmail.com>
2013-10-19 16:39:06 -0400

    Committing initiail rails app

---
+++ QueueMe/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QueueMe::Application.config.secret_token = 'd7f6ce81aec5812c834bcf20e2bd5e18c00604a9b6a32ef1bff1a843e5581a5d6add47e93da98899acc5a9b071bc88f0a6ad32115cfed3374e1f1982b67ba10c'
